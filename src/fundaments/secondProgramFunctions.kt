633856f8b37f1a41d1b28d314ec930cf581434a2
leonardo-mendes <leonardocm92@hotmail.com>
2018-04-12 21:30:30 -0300

    Lessons 25 - 51

--- src/fundaments/secondProgramFunctions.kt
+++ src/fundaments/secondProgramFunctions.kt
@@ -13,6 +13,41 @@ fun main(args: Array<String>) {
     // fun nomeFuncao(param: Tipo) {}
     // fun nomeFuncao() {}
 
-    println(listOf(countNumber(1 ,3), countNumber()))
+    var num1: Int = 2
+    var num2: Int = 3
+
+    var valorMaior: Int = if (num1 < num2) num2 else num1
+
+    // Controle Range, representado entre Valor1..Valor2
+    var valorComRange = if(num1 in num2..num2+10) "Num1 não é maior que Num2+10"  else "Não tem relação"
+
+    var valorComWhen = when(num2){
+        1 -> "O valor do num2 é 1"
+        2, 3 -> "O valor do num2 esta entre 2 e 3"
+        4, 7 -> "O valor do num2 esta entre 4 e 7"
+        in 8..9 -> "O valor do num2 esta entre 8 e 9"
+        else -> "Não é um digito"
+    }
+
+    println("Digite um número: ")
+    while(num1 != 5){
+        val line = readLine() ?: "0"
+        num1 = line.toIntOrNull() ?: 0
+        println("O valor que você digitou foi $num1")
+    }
+
+    // for (i in Valor1..Valor2) -- o i vai percorrer de forma crescente
+    // for (i in Valor1 downTo Valor2) -- o i vai percorrer de forma decrescente
+    // for (i in Valor1..Valor2 step ValorControle) -- o i vai percorrer de forma crescente porem incrementando +ValorControle
+    // for (i in Valor1 downTo Valor2 step ValorControle) -- o i vai percorrer de forma decrescente porem incrementando +ValorControle
+    // for((indice, objetoDoArray) in array.withIndex()){} -- Aqui estamos utilizando o destructuring no for
+
+    println(listOf(
+        countNumber(num1 ,num2),
+        countNumber(),
+        valorMaior,
+        valorComRange,
+        valorComWhen
+    ))
 
 }
---
+++ src/fundaments/thirdProgramOperators.kt
@@ -0,0 +1,26 @@
+package fundaments
+
+data class Carro (val marca: String, val modelo: String)
+
+// Função como ja vimos no arquivo secondProgramFunctions
+fun obterResult(param: Int)  = if(param < 5) "Menor" else "Maior"
+
+fun main(args: Array<String>) {
+
+    // Abaixo vamos utilizar o operador destructuring para retirar variaveis de um objeto ou de um array
+    val (marca , modelo) =  Carro("Ford", "Fiesta")
+    println("Carro na marca $marca e modelo $modelo")
+
+    val (mulher, _ , marido) = listOf("Bela", "Antonio", "José")
+    println("Marido é $marido e Mulher é $mulher")
+
+    // Operadores Lógicos é muito util para gente preencher uma variavel com a resolução de duas funções.
+    val exec1: Boolean = true
+    val exec2: Boolean = true
+
+    val result1 = exec1 || exec2
+    val result2 = exec1 && exec2
+    val result3 = exec1 xor exec2
+    println(listOf(result1, result2, result3, obterResult(6)))
+
+}
